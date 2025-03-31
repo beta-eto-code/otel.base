91415a4699e12278a81b7c38d7cb3c33ae5050c4
Карпин Кирилл <kirill_karpin@hotmail.com>
2025-03-31 11:38:02 +0500

    feat: fix type const

--- src/Interfaces/OTelSpanManagerInterface.php
+++ src/Interfaces/OTelSpanManagerInterface.php
@@ -7,8 +7,8 @@ use OpenTelemetry\SDK\Trace\ReadableSpanInterface;
 
 interface OTelSpanManagerInterface
 {
-    public const string ROOT_SPAN_NAME = 'root';
-    public const string OTEL_TRACER_NAME_DEFAULT = 'io.opentelemetry.contrib.php';
+    public const ROOT_SPAN_NAME = 'root';
+    public const OTEL_TRACER_NAME_DEFAULT = 'io.opentelemetry.contrib.php';
     public function createAndStartSpan(string $name, ?array $attributes = []): void;
 
     public function endSpan(): void;
